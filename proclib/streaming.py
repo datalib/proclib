5120e86b90f808a61d707eb7ed3ef090ce7cdc90
Eugene Eeo <packwolf58@gmail.com>
2015-04-16 22:47:45 +0800

    close file handles to allow SIGPIPE

--- proclib/streaming.py
+++ proclib/streaming.py
@@ -1,3 +1,12 @@
+"""
+    proclib.streaming
+    ~~~~~~~~~~~~~~~~~
+
+    Implements streaming alternatives to the standard
+    Process, Response, and Pipe interfaces.
+"""
+
+
 from subprocess import PIPE
 from .response import Response
 from .process import Process
@@ -5,29 +14,55 @@ from .pipe import Pipe
 
 
 class StreamResponse(Response):
+    """
+    Implements the streaming alternative of the
+    Response object. For a StreamResponse, the
+    wrapped Popen object needn't complete.
+    """
+
     def setup(self):
         pass
 
-    @property
-    def pid(self):
-        return self.process.pid
-
     def wait(self):
+        """
+        Wait for the response to complete.
+        """
         self.process.wait()
 
     def terminate(self):
+        """
+        Terminate the process if possible (if
+        the process hasn't already been killed
+        or exited).
+        """
         if not self.finished:
             self.process.terminate()
 
     @property
     def finished(self):
+        """
+        Returns a boolean telling if the process
+        has completed (exited, killed, etc).
+        """
         return self.returncode is not None
+
+    @property
+    def pid(self):
+        return self.process.pid
 
     @property
     def returncode(self):
         return self.process.poll()
 
     def __enter__(self):
+        """
+        When used as a context manager, the
+        StreamingResponse object yields the
+        response to the caller for convenience.
+        When the block exits, wait for the process
+        to exit and then close the stdout and
+        stderr file objects.
+        """
         return self
 
     def __exit__(self, *_):
@@ -37,6 +72,13 @@ class StreamResponse(Response):
 
 
 class StreamProcess(Process):
+    """
+    Streaming variant of Process, the main
+    difference is that it doesn't ``communicate``
+    with the Popen instance as that will result
+    in reading the entire stdout at once.
+    """
+
     def run(self):
         return StreamResponse(
             command=self.command,
@@ -47,6 +89,12 @@ class StreamProcess(Process):
 
 
 class StreamPipe(Pipe):
+    """
+    Streaming variant of Pipe that uses the
+    ``StreamProcess`` as the default Process
+    class to be used.
+    """
+
     process_class = StreamProcess
 
     def order(self):
@@ -65,3 +113,9 @@ class StreamPipe(Pipe):
             last_stdin = proc.popen.stdout
             procs.append(proc)
         return procs
+
+    def run(self):
+        r = super(StreamPipe, self).run()
+        for item in r.history:
+            item.stdout.close()
+        return r
