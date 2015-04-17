8cf6b7a1e3a98e07e78f91ddedff818338b20964
Eugene Eeo <packwolf58@gmail.com>
2015-04-17 20:24:57 +0800

    document process and response modules [skip ci]

--- proclib/process.py
+++ proclib/process.py
@@ -1,8 +1,26 @@
+"""
+    proclib.process
+    ~~~~~~~~~~~~~~~
+
+    Implements the Process class.
+"""
+
 from subprocess import Popen, PIPE
 from .response import Response
 
 
 class Process(object):
+    """
+    A Process object is a wrapper around a regular
+    `subprocess.Popen` instance that knows how to
+    call it with the correct arguments.
+
+    :param command: The command.
+    :param opts: Keyword argument of additional
+        options to be pased to the `subprocess.Popen`
+        constructor.
+    """
+
     response_cls = Response
     defaults = dict(universal_newlines=True,
                     close_fds=True,
@@ -18,14 +36,26 @@ class Process(object):
         self.process = Popen(args=command, **conf)
 
     def pipe(self, lines):
+        """
+        Given a Process with a valid stdin (not a
+        file-handle that is not PIPE), write *lines*
+        of data to the process where *lines* can
+        be any iterable.
+
+        :param lines: Iterable of data to be piped
+            in to the process.
+        """
         stdin = self.process.stdin
-        if stdin:
-            for line in lines:
-                stdin.write(line)
-            stdin.flush()
-            stdin.close()
+        for line in lines:
+            stdin.write(line)
+        stdin.flush()
+        stdin.close()
 
     def run(self):
+        """
+        Wraps the Popen instance in a ``Response``
+        object.
+        """
         return self.response_cls(
             self.command,
             self.process,
--- proclib/response.py
+++ proclib/response.py
@@ -1,7 +1,23 @@
+"""
+    proclib.response
+    ~~~~~~~~~~~~~~~~
+    Implements the Response class.
+"""
+
+
 from .helpers import cached_property
 
 
 class Response(object):
+    """
+    A Response object represents the result of running
+    a process. The parameters supplied are stored and
+    available as attributes as well.
+
+    :param command: A command in the form of a list.
+    :param process: A `subprocess.Popen` object.
+    """
+
     def __init__(self, command, process):
         self.history = []
         self.command = command
@@ -13,33 +29,63 @@ class Response(object):
 
     @cached_property
     def out(self):
+        """
+        Reads the entire stdout of the Popen instance
+        and then returns it. Not recommended for long
+        running processes.
+        """
         with self.stdout:
             return self.stdout.read()
 
     @cached_property
     def err(self):
+        """
+        Similar to ``out``, reads the entirety of
+        ``stderr``.
+        """
         with self.stderr:
             return self.stderr.read()
 
     @property
     def status_code(self):
+        """
+        Returns the exit code of the process.
+        """
         return self.process.poll()
 
     @property
     def finished(self):
-        return self.status_code is not None
+        """
+        Returns a boolean stating if the process has
+        completed or not.
+        """
+        return self.process.poll() is not None
 
     def close(self):
-        self.stdout.close()
-        self.stderr.close()
+        """
+        If possible, close the stdout and stderr file
+        handles.
+        """
+        if self.stdout: self.stdout.close()
+        if self.stderr: self.stderr.close()
 
     @property
     def ok(self):
+        """
+        Returns a boolean depending on whether the
+        `returncode` attribute equals zero.
+        """
         return self.status_code == 0
 
     def wait(self):
+        """
+        Block until the process to complete.
+        """
         self.process.wait()
 
     def terminate(self):
+        """
+        Terminate the process if it is not finished.
+        """
         if not self.finished:
             self.process.terminate()
